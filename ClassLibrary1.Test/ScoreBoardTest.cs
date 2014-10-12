fb621ecbac5cb69c92d7dbb7cfb8e810b666ce4d
progdojosap <programming.dojo.sapporo@gmail.com>
2014-10-12 15:01:10 +0900

    [from now] 2014/10/12 15:01:10
    diff --git a/ClassLibrary1.Test/ScoreBoardTest.cs b/ClassLibrary1.Test/ScoreBoardTest.cs
    new file mode 100644
    index 0000000..f4a7ecb
    --- /dev/null
    +++ b/ClassLibrary1.Test/ScoreBoardTest.cs
    @@ -0,0 +1,21 @@
    +﻿using NUnit.Framework;
    +using System;
    +using System.Collections.Generic;
    +using System.Linq;
    +using System.Text;
    +using System.Threading.Tasks;
    +
    +namespace ClassLibrary1.Test
    +{
    +    [TestFixture]
    +    public class ScoreBoardTest
    +    {
    +        [Test]
    +        public void ScoreBoardを生成した直後Playerのスコアは0である()
    +        {
    +            int expected = 0;
    +            int actual = new ScoreBoard().getScorePlayer1();
    +            Assert.That(actual, Is.EqualTo(expected));
    +        }
    +    }
    +}
    diff --git a/ClassLibrary1/ScoreBoard.cs b/ClassLibrary1/ScoreBoard.cs
    new file mode 100644
    index 0000000..f4c9a40
    --- /dev/null
    +++ b/ClassLibrary1/ScoreBoard.cs
    @@ -0,0 +1,17 @@
    +﻿using System;
    +using System.Collections.Generic;
    +using System.Linq;
    +using System.Text;
    +
    +namespace ClassLibrary1
    +{
    +    public class ScoreBoard
    +    {
    +        int player1Score = 0;
    +
    +        public int getScorePlayer1()
    +        {
    +            return this.player1Score;
    +        }
    +    }
    +}

---
+++ ClassLibrary1.Test/ScoreBoardTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Test
+{
+    [TestFixture]
+    public class ScoreBoardTest
+    {
+        [Test]
+        public void ScoreBoardを生成した直後Playerのスコアは0である()
+        {
+            int expected = 0;
+            int actual = new ScoreBoard().getScorePlayer1();
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ ClassLibrary1/ScoreBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class ScoreBoard
+    {
+        int player1Score = 0;
+
+        public int getScorePlayer1()
+        {
+            return this.player1Score;
+        }
+    }
+}
